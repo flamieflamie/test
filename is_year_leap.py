f1e836cfcdb1414f59ab74280f8714e44097440d
flamieflamie <flamieflamie@gmail.com>
2016-12-29 16:15:47 +0300

    новый файлик

---
+++ is_year_leap.py
@@ -0,0 +1,19 @@
+def is_year_leap (y):
+    if (y%4 != 0):
+        return "Год не является високосным"
+    else:
+        return "Год является високосным"
+
+ex="Y"
+while (ex=="Y") or (ex=="y"):
+    z=True
+    while z:
+        z=False
+        try:
+            year=int(input("Введите год: \n"))
+        except ValueError:
+            print("Необходимо вводить целое число.")
+            z=True
+ 
+    print(is_year_leap(year))
+    ex=input("Продолжить? (Y/N)\n ")
